4beccbd980af281d7043756deaa712580367bdec
Nikolai <colya.reznick@gmail.com>
2025-04-25 10:17:25 +0300

    Task solution fixed cash

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,5 +1,5 @@
 package mate.academy
 
 fun removeChars(str: String): String {
-    return str.subSequence(1, str.length - 1).toString();
+    return str.subSequence(1, str.length - 1).toString()
 }
